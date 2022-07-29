e86e30069771c244e7710e0bc4d1f2322d84f719
Goksel Coban <goksel326@gmail.com>
2022-07-29 21:46:25 +0300

    add get_suggested_params function

--- algojig/__init__.py
+++ algojig/__init__.py
@@ -1,20 +1,19 @@
 import base64
-from .teal import TealProgram
-from .tealish import TealishProgram
-from .exceptions import LogicEvalError, LogicSigReject
-from .ledger import JigLedger
+
 from algosdk.account import generate_account
 from algosdk.future.transaction import SuggestedParams
 
 
-sp = SuggestedParams(
-    fee=1000,
-    first=1,
-    last=1000,
-    min_fee=1000,
-    flat_fee=True,
-    gh=base64.b64encode(b'\x9b\x01\x08\xe3\xf2Q-6\x1f\xd9\x01z\x9c\x07\x8a`\xe3\x8dR\xc5D\xe9<W\xeb\xd89\xa9\xb9\xdfw@')
-)
+def get_suggested_params():
+    sp = SuggestedParams(
+        fee=1000,
+        first=1,
+        last=1000,
+        min_fee=1000,
+        flat_fee=True,
+        gh=base64.b64encode(b'\x9b\x01\x08\xe3\xf2Q-6\x1f\xd9\x01z\x9c\x07\x8a`\xe3\x8dR\xc5D\xe9<W\xeb\xd89\xa9\xb9\xdfw@')
+    )
+    return sp
 
 
 def generate_accounts(n=10):
